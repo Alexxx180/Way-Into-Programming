8a421550215636d4fa60d0226bcd186cd51bc765
Sasha <alexx120702@gmail.com>
2020-08-28 12:53:17 +0300

    settings

--- new/settings.py
+++ new/settings.py
@@ -36,6 +36,22 @@ ALLOWED_HOSTS = [
 
 # Application definition
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
