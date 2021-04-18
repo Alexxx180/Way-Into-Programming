1beb3b3669c86866ae8dd5244310b3ebc930a62b
Alexxx180 <alexx120702@gmail.com>
2021-04-18 10:50:49 +0300

    WIP final 02

--- main/forms.py
+++ main/forms.py
@@ -1,10 +1,11 @@
 from .models import Players
-from django.forms import ModelForm, TextInput
+from .models import Achievements
+from django.forms import ModelForm, TextInput, Textarea
 
 class PlayersForm(ModelForm):
     class Meta:
         model = Players
-        fields = ["IDplayer","NAMEplayer","LEVELplayer","Level","NEXTexp"]
+        fields = ["IDplayer","NAMEplayer","LEVELplayer","Level","NEXTexp","Code"]
         widgets = {"IDplayer":TextInput(attrs={
             'id':'i1'
             }),
@@ -20,4 +21,46 @@ class PlayersForm(ModelForm):
             }),
             "NEXTexp":TextInput(attrs={
             'id':'i5'
+            }),
+            "Code":Textarea(attrs={
+            'id':'i6'
+            })}
+        widgets
+
+class AchieveForm(ModelForm):
+    class Meta:
+        model = Achievements
+        fields = ["IDp","AchieveBugs","AchieveHome","AchievePull","Achieve5level","Achieve20level","Achieve35level","Achieve50level","AchieveNoob","AchieveBase","AchieveLast"]
+        widgets = {"IDp":TextInput(attrs={
+            'id':'j1'
+            }),
+            "AchieveBugs":TextInput(attrs={
+            'id':'j2'
+            }),
+            "AchieveHome":TextInput(attrs={
+            'id':'j3'
+            }),
+            "AchievePull":TextInput(attrs={
+            'id':'j4'
+            }),
+            "Achieve5level":TextInput(attrs={
+            'id':'j5'
+            }),
+            "Achieve20level":TextInput(attrs={
+            'id':'j6'
+            }),
+            "Achieve35level":TextInput(attrs={
+            'id':'j7'
+            }),
+            "Achieve50level":TextInput(attrs={
+            'id':'j8'
+            }),
+            "AchieveNoob":TextInput(attrs={
+            'id':'j9'
+            }),
+            "AchieveBase":TextInput(attrs={
+            'id':'j10'
+            }),
+            "AchieveLast":TextInput(attrs={
+            'id':'j11'
             })}
--- main/models.py
+++ main/models.py
@@ -1,11 +1,32 @@
 from django.db import models
 
+class Achievements(models.Model):
+    IDp = models.CharField('ID-игрока', max_length=100)
+    AchieveBugs = models.BooleanField(default=0)
+    AchieveHome = models.BooleanField(default=0)
+    AchievePull = models.BooleanField(default=0)
+    Achieve5level = models.BooleanField(default=0)
+    Achieve20level = models.BooleanField(default=0)
+    Achieve35level = models.BooleanField(default=0)
+    Achieve50level = models.BooleanField(default=0)
+    AchieveNoob = models.BooleanField(default=0)
+    AchieveBase = models.BooleanField(default=0)
+    AchieveLast = models.BooleanField(default=0)
+    
+    def __str__(self):
+        return self.IDp
+
+    class Meta:
+        verbose_name='Достижение'
+        verbose_name_plural='Достижения'
+
 class Players(models.Model):
     IDplayer = models.CharField('ID-игрока', max_length=100)
     NAMEplayer = models.CharField('Имя_игрока', max_length=35)
     LEVELplayer = models.IntegerField('Уровень_игрока')
     Level = models.IntegerField('Уровень_локаций')
     NEXTexp = models.IntegerField('Опыт_ДоСледУровня')
+    Code = models.TextField('Код_игрока', max_length=500, null=True)
     
     def __str__(self):
         return self.IDplayer
@@ -13,3 +34,5 @@ class Players(models.Model):
     class Meta:
         verbose_name='Игрок'
         verbose_name_plural='Игроки'
+
+
